333a721e129ab93ae4c90eabc8891ea6776ba7fe
betul <btluzun@gmail.com>
2017-10-25 18:31:00 +0300

    Payment Pages

--- Ticket/assets/js/custom.js
+++ Ticket/assets/js/custom.js
@@ -6,3 +6,14 @@ $("#inpt_search").on('blur', function () {
 	if($(this).val().length == 0)
 		$(this).parent('label').removeClass('active');
 });
+
+
+$('#sidebar-left').click(function(){
+	 if($('.dashboard-content').hasClass('show')){
+		 $('.dashboard-content').removeClass('show');
+		 $('.dashboard-menu-wrapper').removeClass('slide');
+	 }else{
+		 $('.dashboard-content').addClass('show');
+		 $('.dashboard-menu-wrapper').addClass('slide');
+	 }
+ })
