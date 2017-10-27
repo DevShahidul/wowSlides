e6f276b2e0449d965701021a241d72b3ee6efc2c
betul <btluzun@gmail.com>
2017-10-27 19:28:21 +0300

    Sutori Project Updates

--- Ticket/assets/js/custom.js
+++ Ticket/assets/js/custom.js
@@ -1,12 +1,3 @@
-$("#inpt_search").on('focus', function () {
-	$(this).parent('label').addClass('active');
-});
-
-$("#inpt_search").on('blur', function () {
-	if($(this).val().length == 0)
-		$(this).parent('label').removeClass('active');
-});
-
 
 $('#sidebar-left').click(function(){
 	 if($('.dashboard-content').hasClass('show')){
