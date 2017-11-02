f55b9a2f69acd3a79abd5c7903b9678ce905afa7
betul <btluzun@gmail.com>
2017-11-02 18:42:48 +0300

    Single Payment Page

--- Ticket/assets/js/custom.js
+++ Ticket/assets/js/custom.js
@@ -8,3 +8,20 @@ $('#sidebar-left').click(function(){
 		 $('.dashboard-menu-wrapper').addClass('slide');
 	 }
  })
+
+function sidebarControl() {
+	if($( window ).width() < 768) {
+			$('.dashboard-menu-wrapper').removeClass('small-dashboard');
+	}
+	else {
+		 $('.dashboard-menu-wrapper').addClass('small-dashboard');
+	}
+}
+
+ $( window ).resize(function() {
+	 sidebarControl();
+ });
+
+ $( document ).ready(function() {
+	sidebarControl();
+ });
