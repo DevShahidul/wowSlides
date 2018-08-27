194bc60a4891df91fb7ea5f6fcc2fc5ca6fc55ee
betul <btluzun@gmail.com>
2018-08-27 16:05:47 +0300

    Kiosk Login Page - Added Keyboard

--- Ticket/tofasTv/app.js
+++ Ticket/tofasTv/app.js
@@ -25,4 +25,93 @@ app.controller("personCourseList", function($scope) {
     };
 });
 
-var appCtrl = angular.module("appCtrl", []);
+var appCtrl = angular.module("appCtrl", ['ng-virtual-keyboard']);
+
+
+appCtrl.config(['VKI_CONFIG', function(VKI_CONFIG){
+  VKI_CONFIG.position = {
+    of: null,
+    my: 'center top',
+    at: 'center top',
+    at2: 'center bottom'
+  };
+  VKI_CONFIG.tabNavigation = true;
+  VKI_CONFIG.css = {
+    container: 'ui-widget-content ui-widget ui-corner-all ui-helper-clearfix',
+    buttonDefault: 'ui-state-default ui-corner-all',
+       // hovered button
+       buttonHover: 'ui-state-hover',
+       // Action keys (e.g. Accept, Cancel, Tab, etc);
+       // this replaces "actionClass" option
+       buttonAction: 'ui-state-active',
+       // Active keys
+       // (e.g. shift down, meta keyset active, combo keys active)
+       buttonActive: 'ui-state-active',
+       // used when disabling the decimal button {dec}
+       // when a decimal exists in the input area
+       buttonDisabled: 'ui-state-disabled',
+       // {empty} button class name
+       buttonEmpty: 'ui-keyboard-empty'
+  };
+  VKI_CONFIG.extensions = {
+    addTyping: false
+  };
+}])
+
+appCtrl.controller('ExampleCtrl', ['$scope', 'ngVirtualKeyboardService', function($scope, ngVirtualKeyboardService){
+  $scope.t01Config = {
+    layout: 'alpha',
+    extensions: {
+      addTyping: false
+    }
+  };
+
+  $scope.t04Config = {
+    layout: 'international',
+    autoAccept: true,
+    extensions: {
+      addScramble: {randomizeOnce: false},
+      addTyping: false
+    }
+  };
+
+  $scope.onKeyboardClick = function(id){
+    var keyboard = ngVirtualKeyboardService.getKeyboardById(id);
+    if (keyboard) {
+      if (keyboard.isOpen) {
+        keyboard.close();
+      } else {
+        keyboard.reveal();
+      }
+    }
+  };
+
+  $scope.t05Config = {
+    layout: 'alpha',
+    extensions: {
+      addTyping: false
+    },
+    events: {
+      keyboardChange: function(e, kb, el) {
+        console.log(e.type + ', "' + el.value + '", "' + kb.$preview.val() + '"');
+      },
+      change: function(e, kb, el) {
+        console.log(e.type + ', "' + el.value + '", "' + kb.$preview.val() + '"');
+      }
+    }
+  };
+
+  $scope.t06Config = {
+    layout: 'alpha',
+    extensions: {
+      addTyping: false
+    },
+    autoAccept: true,
+    usePreview: false,
+    autoUpdateModel: true
+  };
+
+  $scope.t06Change = function() {
+    console.log('t06 value changed');
+  };
+}]);
