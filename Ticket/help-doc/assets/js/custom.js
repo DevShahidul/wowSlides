a833fd4addd3f7b79a5a76f781a049d367e1d953
betul <btluzun@gmail.com>
2019-02-13 11:31:03 +0300

    Help Doc Update

--- Ticket/help-doc/assets/js/custom.js
+++ Ticket/help-doc/assets/js/custom.js
@@ -29,11 +29,19 @@ $(document).ready(function(){
 
 
 	// Sidebar Trigger
-	$('#sidebar-toggle').click(function(){
-	     $('#sidebar').css('right', '0')
-	});
-	$('.close-right').click(function(){
-	     $('#sidebar').css('right', '-300px')
+	$('.leftmenu-trigger').click(function(){
+		if ( $('#leftsidebar').hasClass("triggered-sidebar") ) {   
+	      $('#leftsidebar').removeClass("triggered-sidebar");
+	      $('.leftmenu-trigger').removeClass("triggered");
+	      $('#doc-content').addClass("col-xl-8").removeClass("col-xl-10");
+	 	}
+	 	else {
+	 	  $('#leftsidebar').addClass("triggered-sidebar");
+	 	  $('.leftmenu-trigger').addClass("triggered");
+	 	  $('.leftmenu-trigger').addClass("triggered");
+	 	  $('#doc-content').addClass("col-xl-10").removeClass("col-xl-8");
+
+	 	}
 	});
 
 });
